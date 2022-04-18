985fd5da0264788fbce2f03226ae1e8be1e5747d
hansy0 <hansy@ihateflyingbugs.com>
2022-04-18 20:38:50 +0900

    update 003

--- 003.py
+++ 003.py
@@ -2,16 +2,19 @@
 
 def solution(N): 
     factors = []
-    if N == 1 :
-        factors.append(N)
-
-    else:
-        for x in range(2, N):
+    M=N
+    if N ==1 :
+        factors.append(1)
+    else : 
+        x = 2 
+        while x * x <= M :
             while N % x ==0 :
-                factors.append(x)
-                N = N//x
+                if x not in factors:
+                    factors.append(x)
+                N = N/x
+            x+=1
     
     return max(factors)
 
 
-solution(14)
+solution(600851475143)
