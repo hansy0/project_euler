b9243600e2dee07a3814cee33f9e4be298c5fa5e
hansy0 <hansy@ihateflyingbugs.com>
2022-04-20 21:21:09 +0900

    update 002 using generator

--- 002.py
+++ 002.py
@@ -1,13 +1,27 @@
-#002
+#002 4백만 이하 짝수 값을 갖는 항의 합
 
-def solution():
-    x1=1
-    x2=2
-    fibonacci = []
-    while x2 <40000000:
-        y=x1 + x2
-        if y%2 ==0 : 
-            fibonacci.append(y)
-    return sum(fibonacci)
+def fibonacci(N):
+    i = 1 #피보나치수
+    j = 1 
+    while i < N :
+        yield i 
+        i, j = i+j , i
 
-solution()
+result = sum([x for x in list(fibonacci(4000000)) if x%2 ==0])
+print(result)
+
+
+# def solution():
+#     x1=1
+#     x2=2
+#     fibonacci = [1,2]
+#     while x2 <= 4000000:
+#         y=x1 + x2
+#         fibonacci.append(y)
+#         x1, x2 = x2, y
+#     return sum(x for x in fibonacci if x%2==0)
+# 
+
+#solution()
+#---------------------
+# answer = 4613732
