7ea525b66d2fd4d26d3ab788cc20e0c91a946450
hansy0 <hansy@ihateflyingbugs.com>
2022-04-20 21:59:16 +0900

    update 003 using generator

--- 003.py
+++ 003.py
@@ -1,20 +1,38 @@
-#003
+#003 
+# 가장 큰 소인수 구하기
 
-def solution(N): 
-    factors = []
-    M=N
-    if N ==1 :
-        factors.append(1)
-    else : 
-        x = 2 
-        while x * x <= M :
-            while N % x ==0 :
-                if x not in factors:
-                    factors.append(x)
-                N = N/x
-            x+=1
-    
-    return max(factors)
+# 소인수  
+def factor_generator(N):
+    i = 2
+    while N != 1:
+        if N%i ==0:
+            yield i 
+            N = N/i
+        i +=1
+        
 
 
-solution(600851475143)
+result = max(x for x in list(factor_generator(600851475143)))
+print(result)
+
+
+#def solution(N): 
+#    factors = []
+#    M=N
+#    if N ==1 :
+#        factors.append(1)
+#    else : 
+#        x = 2 
+#        while x * x <= M :
+#            while N % x ==0 :
+#                if x not in factors:
+#                    factors.append(x)
+#                N = N/x
+#            x+=1
+#    
+#    return max(factors)
+#
+#
+#solution(600851475143)
+#---------------------
+# answer = 6857
